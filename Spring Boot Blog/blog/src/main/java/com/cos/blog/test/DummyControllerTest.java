0edb43f90f844dd408d4aa6b9a1feb9f18ed969a
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-01-20 01:16:12 +0900

    delete

--- Spring Boot Blog/blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ Spring Boot Blog/blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,49 +29,66 @@ public class DummyControllerTest {
 	@Autowired // 의존성 주입
 	private UserRepository userRepository;
 
+	// delete
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "삭제 실패. 해당 id는 저장되어 있지 않습니다.";
+		}
+		return "삭제되었습니다. id : " + id;
+	}
+
+	// insert
 	// save함수는 id를 전달하지 않으면 insert를 진행
 	// save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update 진행
 	// save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert 진행
 	// email, password
-	// http://localhost:8000/blog/dummy/user/{id} 
+	// http://localhost:8000/blog/dummy/user/{id}
 	@Transactional // 더티 체킹, 함수 종료시에 자동 commit이 됨, 변경을 감지하고 데이터베이스에 수정 요청
 	@PutMapping("/dummy/user/{id}")
-	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //JSON 데이터를 요청 => JAVA OBJECT(MESSAGECONVERTOR의 JACKSON 라이브러리가 변환해서 받음
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { // JSON 데이터를 요청 => JAVA
+																					// OBJECT(MESSAGECONVERTOR의 JACKSON
+																					// 라이브러리가 변환해서 받음
 		System.out.println("id : " + id);
 		System.out.println("password : " + requestUser.getPassword());
 		System.out.println("email : " + requestUser.getEmail());
-		
-		User user = userRepository.findById(id).orElseThrow(()->{
+
+		User user = userRepository.findById(id).orElseThrow(() -> {
 			return new IllegalArgumentException("수정에 실패했습니다.");
 		}); // 영속화 단계
-		
+
 		user.setPassword(requestUser.getPassword()); // 영속화 이후 데이터 변경
 		user.setEmail(requestUser.getEmail()); // 영속화 이후 데이터 변경
-		
-		// userRepository.save(user); // @Transactional을 통해 해당 라인이 없어도 데이터베이스에 수정된 데이터 반영 가능
-		
+
+		// userRepository.save(user); // @Transactional을 통해 해당 라인이 없어도 데이터베이스에 수정된 데이터
+		// 반영 가능
+
 		// 더티 체킹
-		
-		return null;
-		
+
+		return user;
 	}
-	
+
+	// select
 	// http://localhost:8000/blog/dummy/users
 	@GetMapping("/dummy/users")
 	public List<User> list() {
 		return userRepository.findAll();
 	}
 
+	//paging
 	// 한페이지당 2건에 데이터를 리턴받아 볼 예정
 	// http://localhost:8000/blog/dummy/user
 	@GetMapping("/dummy/user")
-	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+	public List<User> pageList(
+			@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 		Page<User> pagingUser = userRepository.findAll(pageable);
-		
+
 		List<User> users = pagingUser.getContent();
 		return users;
 	}
-	
+
 	/*
 	 * // {id} 주소로 파라미터를 전달 받을 수 있음 // http://localhost:8000/blog/dummy/user/3
 	 * 
@@ -85,6 +104,7 @@ public class DummyControllerTest {
 	 * }); return user; }
 	 */
 
+	// select
 	// {id} 주소로 파라미터를 전달 받을 수 있음
 	// http://localhost:8000/blog/dummy/user/3
 	@GetMapping("/dummy/user/{id}")
@@ -109,7 +129,6 @@ public class DummyControllerTest {
 		// user 오브젝트를 json으로 변환해서 브라우저에게 보냄
 		return user;
 	}
-
 	/*
 	 * // {id} 주소로 파라미터를 전달 받을 수 있음 // http://localhost:8000/blog/dummy/user/3
 	 * 
@@ -122,6 +141,7 @@ public class DummyControllerTest {
 	 * IllegalArgumentException("해당 사용자는 없습니다."); }); return user; }
 	 */
 
+	// insert
 	/*
 	 * http://localhost:8000/blog/dummy/join (요청) http의 body에 username, password,
 	 * email 데이터를 가지고 (요청)
