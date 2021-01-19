385d761116b81f4e79c579cf969ad6d61143bd09
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-01-20 00:45:41 +0900

    데이터 수정

--- Spring Boot Blog/blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ Spring Boot Blog/blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -8,9 +8,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -24,6 +27,33 @@ public class DummyControllerTest {
 	@Autowired // 의존성 주입
 	private UserRepository userRepository;
 
+	// save함수는 id를 전달하지 않으면 insert를 진행
+	// save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update 진행
+	// save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert 진행
+	// email, password
+	
+	@Transactional // 더티 체킹
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //JSON 데이터를 요청 => JAVA OBJECT(MESSAGECONVERTOR의 JACKSON 라이브러리가 변환해서 받음
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패했습니다.");
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		// userRepository.save(user); // @Transactional을 통해 해당 라인이 없어도 데이터베이스에 수정된 데이터 반영 가능
+		
+		// 더티 체킹
+		
+		return null;
+		
+	}
+	
 	// http://localhost:8000/blog/dummy/users
 	@GetMapping("/dummy/users")
 	public List<User> list() {
