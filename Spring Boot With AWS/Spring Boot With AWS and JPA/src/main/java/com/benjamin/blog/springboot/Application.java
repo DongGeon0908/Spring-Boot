62a3398a3b3dedcd8e74bc1001ed414cc65af199
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-02-16 00:22:06 +0900

    google login Oauth2 Version 2.0 API

--- Spring Boot With AWS/Spring Boot With AWS and JPA/src/main/java/com/benjamin/blog/springboot/Application.java
+++ Spring Boot With AWS/Spring Boot With AWS and JPA/src/main/java/com/benjamin/blog/springboot/Application.java
@@ -10,6 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
 public class Application {
     public static void main(String[] args){
-       SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
--- Spring Boot With AWS/Spring Boot With AWS and JPA/src/main/java/com/benjamin/blog/springboot/config/auth/SecurityConfig.java
+++ Spring Boot With AWS/Spring Boot With AWS and JPA/src/main/java/com/benjamin/blog/springboot/config/auth/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2
 @EnableWebSecurity // Spring Security 설정등을 활성화시킴
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CustomUserTypesOAuth2UserService customUserTypesOAuth2UserService;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,6 +29,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .oauth2Login()
                         .userInfoEndpoint()
-                            .userService(customUserTypesOAuth2UserService);
+                            .userService(customOAuth2UserService);
     }
 }
