59cbcae10783ea8047bfbc2b50e267b3bb0946f6
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-02-22 23:42:50 +0900

    JPA TEST - SELECT, INSERT, UPDATE, DELETE

---
+++ ex2/src/test/java/com/zerock/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,73 @@
+package com.zerock.ex2.repository;
+
+import com.zerock.ex2.entity.Memo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testClass(){
+        System.out.println(memoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsertDummies(){
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("Sample..." + i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+    @Test
+    public void testSelect(){
+
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("=============================================");
+
+        if(result.isPresent()){
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testSelect2(){
+        // 데이터베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Memo memo = memoRepository.getOne(mno);
+
+        System.out.println("=================================");
+
+        System.out.println(memo);
+    }
+
+    @Test
+    public void testUpdate(){
+        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+
+        System.out.println(memoRepository.save(memo));
+    }
+
+    @Test
+    public void testDelete(){
+        Long mno = 100L;
+
+        memoRepository.deleteById(mno);
+    }
+}
