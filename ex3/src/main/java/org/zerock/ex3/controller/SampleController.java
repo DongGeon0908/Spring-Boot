129e88e4d20e06b05f3797e339ccf13c1610bfdc
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-02-23 22:31:45 +0900

    Thymeleaf 반복문

---
+++ ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -0,0 +1,39 @@
+package org.zerock.ex3.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.ex3.dto.SampleDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j2
+public class SampleController {
+
+    @GetMapping("/ex1")
+    public void ex1(){
+        log.info("ex1......................");
+    }
+
+    @GetMapping({"/ex2"})
+    public void exModel(Model model){
+        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream().mapToObj(i -> {
+            SampleDTO dto = SampleDTO.builder()
+                    .sno(i)
+                    .first("First.."+i)
+                    .last("Last.."+i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+            return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
+    }
+}
---
+++ ex3/src/main/java/org/zerock/ex3/dto/SampleDTO.java
@@ -0,0 +1,19 @@
+package org.zerock.ex3.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+}
