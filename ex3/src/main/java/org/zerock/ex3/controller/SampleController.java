eb509fe45d56a3bda84f8a188d475ab88e6bd5b9
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-02-24 00:27:47 +0900

    thymeleaf with bootstrap

--- ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
+++ ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -59,4 +59,9 @@ public class SampleController {
     public void ex3(){
         log.info("ex3");
     }
+
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate", "/exSidebar"})
+    public void exLayout1() {
+        log.info("exLayout..............");
+    }
 }
