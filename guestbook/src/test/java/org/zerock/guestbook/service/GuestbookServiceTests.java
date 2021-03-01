742ffcfcbede951e280af1212e8256dc3a594f21
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-01 17:28:28 +0900

    JPA with DTO for Page

--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -33,8 +33,18 @@ public class GuestbookServiceTests {
 
         PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
 
+        System.out.println("PREV : " + resultDTO.isPrev());
+        System.out.println("NEXT : " + resultDTO.isNext());
+        System.out.println("TOTAL : " + resultDTO.getTotalPage());
+
+        System.out.println("--------------------------------------------------------");
         for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
             System.out.println(guestbookDTO);
         }
+
+        System.out.println("--------------------------------------------------------");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+
+
     }
 }
