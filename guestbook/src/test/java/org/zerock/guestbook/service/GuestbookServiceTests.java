b2951bfdfe9c603e8e711e63b2f0f022e9fd8e26
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-03 22:23:31 +0900

    Search Data Test

--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -45,6 +45,30 @@ public class GuestbookServiceTests {
         System.out.println("--------------------------------------------------------");
         resultDTO.getPageList().forEach(i -> System.out.println(i));
 
+    }
 
+    @Test
+    public void testSearch(){
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("tc") // 검색 조건 -> t, c, w, tc, tcw
+                .keyword("한글") // Search Keyword
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("--------------------------------------------------------");
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("--------------------------------------------------------");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
 }
