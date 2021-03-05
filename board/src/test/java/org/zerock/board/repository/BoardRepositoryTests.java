1dd624ad7ce6b8d55d05c081e26065dfec9f612f
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-05 16:15:37 +0900

    TDD - BoardRepository Left Outer Join TEST

--- board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -29,5 +30,17 @@ public class BoardRepositoryTests {
 
             boardRepository.save(board);
         });
+    }
+    
+    @Test
+    public void testRead1(){
+
+        // 데이터베이스에 존재하는 번호
+        Optional<Board> result = boardRepository.findById(100L);
+
+        Board board = result.get();
+
+        System.out.println(board);
+        System.out.println(board.getWriter());
     }
 }
