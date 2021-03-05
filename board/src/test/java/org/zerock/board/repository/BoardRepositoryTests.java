cff8c4fb0954435207e74642f2bb5bcb1bdc1a3f
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-05 16:09:07 +0900

    TDD - InsertBoard TEST

--- board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -1,4 +1,33 @@
 package org.zerock.board.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.Member;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
 public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void insertBoard(){
+
+        IntStream.rangeClosed(1,100).forEach(i -> {
+
+            Member member = Member.builder().email("user" + i + "@aaa.com").build();
+
+            Board board = Board.builder()
+                    .title("Title..." + i)
+                    .content("Content...." + i)
+                    .writer(member)
+                    .build();
+
+            boardRepository.save(board);
+        });
+    }
 }
