aa9ac178ab5702d154225fd0bb9cceb8fe9d0fb7
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-05 16:52:38 +0900

    TDD - JPQL - BoardRepository

--- board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -3,9 +3,14 @@ package org.zerock.board.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -67,5 +72,20 @@ public class BoardRepositoryTests {
         for(Object[] arr : result){
             System.out.println(Arrays.toString(arr));
         }
+    }
+
+    @Test
+    public void testWithReplyCount(){
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row -> {
+
+            Object[] arr = (Object[])row;
+
+            System.out.println(Arrays.toString(arr));
+        });
     }
 }
