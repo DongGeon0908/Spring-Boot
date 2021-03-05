79fc80cacd40a3c2b3269df15d038bb8e1aeff2a
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-05 16:17:53 +0900

    TDD - ReplyRepository Left Outer Join TEST

--- board/src/test/java/org/zerock/board/repository/ReplyRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/ReplyRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -30,5 +31,16 @@ public class ReplyRepositoryTests {
 
             replyRepository.save(reply);
         });
+    }
+
+    @Test
+    public void readReply1(){
+
+        Optional<Reply> result = replyRepository.findById(1L);
+
+        Reply reply = result.get();
+
+        System.out.println(reply);
+        System.out.println(reply.getBoard());
     }
 }
