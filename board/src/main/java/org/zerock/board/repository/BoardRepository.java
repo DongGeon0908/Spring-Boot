84375270ec52e50c338ba10faf160454ad158dec
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-05 16:41:57 +0900

    TDD - JPQL TEST

--- board/src/main/java/org/zerock/board/repository/BoardRepository.java
+++ board/src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -1,7 +1,17 @@
 package org.zerock.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.board.entity.Board;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    // 한 개의 로우(Object) 내에 Object[]로 나옴
+    @Query("select b, w from Board b left join b.writer w where b.bno =:bno")
+    Object getBoardWithWriter(@Param("bno") Long bno);
+
+    @Query("select b, r from Board b left join Reply r on r.board = b where b.bno = :bno")
+    List<Object[]> getBoardWithReply(@Param("bno") Long bno);
 }
--- board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -7,6 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -44,5 +46,26 @@ public class BoardRepositoryTests {
 
         System.out.println(board);
         System.out.println(board.getWriter());
+    }
+
+    @Test
+    public void testReadWithWriter(){
+
+        Object result = boardRepository.getBoardWithWriter(100L);
+
+        Object[] arr = (Object[])result;
+
+        System.out.println("-----------------------------------------------");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void testGetBoardWithReply() {
+
+        List<Object[]> result = boardRepository.getBoardWithReply(100L);
+
+        for(Object[] arr : result){
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
