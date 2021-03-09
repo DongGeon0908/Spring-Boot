7aa69e26d4564d1451bd8fc2a8302955ceeaca2d
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-09 13:00:37 +0900

    Insert Board with View

--- board/src/main/java/org/zerock/board/controller/BoardController.java
+++ board/src/main/java/org/zerock/board/controller/BoardController.java
@@ -5,7 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.board.dto.BoardDTO;
 import org.zerock.board.dto.PageRequestDTO;
 import org.zerock.board.service.BoardService;
 
@@ -23,5 +26,25 @@ public class BoardController {
         log.info("list..............." + pageRequestDTO);
 
         model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
+
+        log.info("dto...." + dto);
+
+        // 새로 추가된 엔티티의 번호
+        Long bno = boardService.register(dto);
+
+        log.info("BNO : " + bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
     }
 }
