41026be09c45fcf5e4a0fa03e3c57ccaba50d7b0
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-09 13:14:58 +0900

    Board update/delete

--- board/src/main/java/org/zerock/board/controller/BoardController.java
+++ board/src/main/java/org/zerock/board/controller/BoardController.java
@@ -60,4 +60,33 @@ public class BoardController {
 
         model.addAttribute("dto", boardDTO);
     }
+
+    @PostMapping("/remove")
+    public String remove(long bno, RedirectAttributes redirectAttributes){
+
+        log.info("bno: " + bno);
+
+        boardService.removeWithReplies(bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+    }
+
+    @PostMapping("/modify")
+    public String modify(BoardDTO dto, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, RedirectAttributes redirectAttributes){
+
+        log.info("post modify.......................................");
+        log.info("dto: " + dto);
+
+        boardService.modify(dto);
+
+        redirectAttributes.addAttribute("page", requestDTO.getPage());
+        redirectAttributes.addAttribute("type", requestDTO.getType());
+        redirectAttributes.addAttribute("keyword", requestDTO.getKeyword());
+
+        redirectAttributes.addAttribute("bno", dto.getBno());
+
+        return "redirect:/board/read";
+    }
 }
