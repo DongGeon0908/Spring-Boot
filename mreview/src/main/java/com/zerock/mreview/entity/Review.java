ee55e5e858bb035fbff3f0c3ebf8abf857b2bdcf
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-11 11:25:37 +0900

    Entity Class With M:N

---
+++ mreview/src/main/java/com/zerock/mreview/entity/Review.java
@@ -0,0 +1,28 @@
+package com.zerock.mreview.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"movie", "member"})
+public class Review extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewnum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    private int grade;
+
+    private String text;
+}
