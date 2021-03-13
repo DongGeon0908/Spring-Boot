097a3e6c51ecf01c92a46ef977ce97ccdeb476f8
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-13 14:44:29 +0900

    JPQL

--- mreview/src/main/java/com/zerock/mreview/repository/MovieRepository.java
+++ mreview/src/main/java/com/zerock/mreview/repository/MovieRepository.java
@@ -1,7 +1,15 @@
 package com.zerock.mreview.repository;
 
 import com.zerock.mreview.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @Query("select m, mi, avg(coalesce(r.grade,0)), count(distinct r) from Movie m " +
+            "left outer join MovieImage mi on mi.movie = m " +
+            "left outer join Review r on r.movie = m group by m")
+    Page<Object[]> getListPage(Pageable pageable);
 }
--- mreview/src/test/java/com/zerock/mreview/repository/MovieRepositoryTests.java
+++ mreview/src/test/java/com/zerock/mreview/repository/MovieRepositoryTests.java
@@ -5,9 +5,13 @@ import com.zerock.mreview.entity.MovieImage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -46,5 +50,17 @@ public class MovieRepositoryTests {
 
             System.out.println("============================================");
         });
+    }
+
+    @Test
+    public void testListPage(){
+
+        PageRequest pageRequest = PageRequest.of(0,10, Sort.by(Sort.Direction.DESC, "mno"));
+
+        Page<Object[]> result = movieRepository.getListPage(pageRequest);
+
+        for(Object[] objects : result.getContent()) {
+            System.out.println(Arrays.toString(objects));
+        }
     }
 }
