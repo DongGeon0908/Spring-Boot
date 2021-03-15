d6bdcd5497cb9d991a992e58259fbe2f8e1b0592
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-15 11:27:55 +0900

    MIME Type에 따라 파일 데이터의 처리

--- mreview/src/main/java/com/zerock/mreview/controller/UploadController.java
+++ mreview/src/main/java/com/zerock/mreview/controller/UploadController.java
@@ -1,24 +1,118 @@
 package com.zerock.mreview.controller;
 
+
+import com.zerock.mreview.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @Log4j2
 public class UploadController {
 
+    @Value("${org.zerock.upload.path}") // application.properties의 변수
+    private String uploadPath;
+
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles){
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for(MultipartFile uploadFile : uploadFiles){
+
+            // 이미지 파일만 업로드 가능
+            if(uploadFile.getContentType().startsWith("image") == false){
+                log.warn("this file is not image type");
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
 
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어옴
             String originalName = uploadFile.getOriginalFilename();
             String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
 
             log.info("fileName: " + fileName);
+
+            // 날짜 폴더 생성
+            String folderPath = makeFolder();
+
+            // UUID
+            String uuid = UUID.randomUUID().toString();
+
+            // 저장할 파일 이름 중간에 "_"를 이용해서 구분
+            String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+
+            Path savePath = Paths.get(saveName);
+
+            try{
+                uploadFile.transferTo(savePath); // 실제 이미지 저장
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
+            } catch (IOException e) {
+                e.printStackTrace();;
+            }
+
         } // end for
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName){
+
+        ResponseEntity<byte[]> result = null;
+
+        try{
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+
+            log.info("fileName: " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME 타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file),header, HttpStatus.OK);
+        } catch (Exception e){
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
+    }
+
+    private String makeFolder() {
+
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("//", File.separator);
+
+        // make folder
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if(uploadPathFolder.exists() == false){
+            uploadPathFolder.mkdirs();
+        }
+        return folderPath;
     }
 }
