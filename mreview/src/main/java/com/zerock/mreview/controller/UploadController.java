aa6d64f31393fc94b26c48d0766e62dc64270843
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-15 10:31:03 +0900

    File Upload Ajax

---
+++ mreview/src/main/java/com/zerock/mreview/controller/UploadController.java
@@ -0,0 +1,24 @@
+package com.zerock.mreview.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@Log4j2
+public class UploadController {
+
+    @PostMapping("/uploadAjax")
+    public void uploadFile(MultipartFile[] uploadFiles){
+
+        for(MultipartFile uploadFile : uploadFiles){
+
+            // 실제 파일 이름 IE나 Edge는 전체 경로가 들어옴
+            String originalName = uploadFile.getOriginalFilename();
+            String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+
+            log.info("fileName: " + fileName);
+        } // end for
+    }
+}
