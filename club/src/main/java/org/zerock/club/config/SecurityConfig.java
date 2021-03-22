4605340df915c15a9368dcd1dde923cbcb7b453d
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-22 19:27:12 +0900

    HttpSecurity 타입 메서드 Override를 통한 접근제한 설정

--- club/src/main/java/org/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,4 +28,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .password("$2a$10$tvL31cpZvHo2A.JtVzgnleniEM.Wr3lrjdPYHhBBLpY/4fNq.Mn6u").roles("USER");
 
     }
+    
+    // HttpSecurity 타입으로 접근 제한 처리
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll();
+    }
+
 }
