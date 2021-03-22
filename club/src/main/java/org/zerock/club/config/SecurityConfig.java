5e7460ca21419827859c2e6a88d2870558baba26
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-22 19:21:42 +0900

    BCryptPasswordEncoder Test

--- club/src/main/java/org/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -1,10 +1,30 @@
 package org.zerock.club.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // 사용자 계정은 user1
+        auth.inMemoryAuthentication().withUser("user1")
+
+        // 1111 패스워드 인코딩 결과
+        .password("$2a$10$tvL31cpZvHo2A.JtVzgnleniEM.Wr3lrjdPYHhBBLpY/4fNq.Mn6u").roles("USER");
+
+    }
 }
