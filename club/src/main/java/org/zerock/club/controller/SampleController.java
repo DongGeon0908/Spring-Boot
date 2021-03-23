2ca7085b5640992f9867b3693540b26da509b157
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-23 12:17:27 +0900

    기본적인 스프링 시큐리 사용법

--- club/src/main/java/org/zerock/club/controller/SampleController.java
+++ club/src/main/java/org/zerock/club/controller/SampleController.java
@@ -1,9 +1,11 @@
 package org.zerock.club.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.club.security.dto.ClubAuthMemberDTO;
 
 @Controller
 @Log4j2
@@ -16,8 +18,12 @@ public class SampleController {
     }
 
     @GetMapping("/member")
-    public void exMember(){
+    public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMember){
         log.info("exMember...........");
+
+        log.info("-----------------------------------------");
+
+        log.info(clubAuthMember);
     }
 
     @GetMapping("/admin")
