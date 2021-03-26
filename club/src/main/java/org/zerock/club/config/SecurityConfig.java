484ec483a4960280db4fd051de1e6aa963b1e53d
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-26 17:12:24 +0900

    접근제한 수정

--- club/src/main/java/org/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -1,19 +1,25 @@
 package org.zerock.club.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.zerock.club.security.handler.ClubLoginSuccessHandler;
+import org.zerock.club.security.service.ClubUserDetailsService;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private ClubUserDetailsService userDetailsService; // 주입
+    
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -23,15 +29,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
-                .antMatchers("/sample/all").permitAll()
-                .antMatchers("/sample/member").hasRole("USER");
+        //http.authorizeRequests()
+        //        .antMatchers("/sample/all").permitAll()
+        //        .antMatchers("/sample/member").hasRole("USER");
 
         // 인가 및 인증에서 문제가 발생하면 로그인 화면으로 전환
         http.formLogin();
         http.csrf().disable();
-
+        http.logout();
         http.oauth2Login().successHandler(successHandler());
+        http.rememberMe().tokenValiditySeconds(60*60*7).userDetailsService(userDetailsService); // 7days
     }
 
     @Bean
--- club/src/main/java/org/zerock/club/controller/SampleController.java
+++ club/src/main/java/org/zerock/club/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.club.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.zerock.club.security.dto.ClubAuthMemberDTO;
 @RequestMapping("/sample/")
 public class SampleController {
 
+    @PreAuthorize("permitAll()")
     @GetMapping("/all")
     public void exAll(){
         log.info("exAll............");
@@ -26,6 +28,7 @@ public class SampleController {
         log.info(clubAuthMember);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public void exAdmin(){
         log.info("exAdmin...........");
