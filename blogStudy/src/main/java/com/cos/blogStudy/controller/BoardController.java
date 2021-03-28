f697eab6a57d19a74b3f900d2750f461662cfbf3
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-29 08:49:38 +0900

    index page 구성

---
+++ blogStudy/src/main/java/com/cos/blogStudy/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.cos.blogStudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	
+	@GetMapping({"","/"})
+	public String index() {
+		return "index";
+	}
+}
---
+++ blogStudy/src/main/java/com/cos/blogStudy/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.cos.blogStudy.controller;
+
+public class UserController {
+
+}
