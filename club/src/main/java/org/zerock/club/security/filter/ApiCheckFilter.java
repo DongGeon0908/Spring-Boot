a188cbc94ea110b616d11b001564bed140a541f8
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-30 00:13:56 +0900

    Authorization Header 처리

--- club/src/main/java/org/zerock/club/security/filter/ApiCheckFilter.java
+++ club/src/main/java/org/zerock/club/security/filter/ApiCheckFilter.java
@@ -1,7 +1,9 @@
 package org.zerock.club.security.filter;
 
 import lombok.extern.log4j.Log4j2;
+import net.minidev.json.JSONObject;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Log4j2
 public class ApiCheckFilter extends OncePerRequestFilter {
@@ -35,10 +38,46 @@ public class ApiCheckFilter extends OncePerRequestFilter {
             log.info("ApiCheckFilter............................................");
             log.info("ApiCheckFilter............................................");
 
-            return;
+            boolean checkHeader = checkAuthHeader(request);
+
+            if(checkHeader){
+                filterChain.doFilter(request, response);
+                return;
+            } else{
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+                // json 리턴 및 한글깨짐 수정
+                response.setContentType("application/json;charset=utf-8");
+                JSONObject json = new JSONObject();
+                String message = "FAIL CHECK API TOKEN";
+                json.put("code", "403");
+                json.put("message", message);
+
+                PrintWriter out = response.getWriter();
+                out.print(json);
+                return;
+
+            }
         }
 
         filterChain.doFilter(request, response);
 
+    }
+
+    // doFilterInternal()에서 다음 필터로 진행할 것인지 결정
+    private boolean checkAuthHeader(HttpServletRequest request){
+
+        boolean checkResult = false;
+
+        String authHeader = request.getHeader("Authorization");
+
+        if(StringUtils.hasText(authHeader)){
+            log.info("Authorization exist : " + authHeader);
+            if(authHeader.equals("12345678")){
+                checkResult = true;
+            }
+        }
+
+        return checkResult; 
     }
 }
