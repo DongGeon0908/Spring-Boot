ae23d2e263ba33bad1d151849c40792e8ef026df
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-03-31 19:55:22 +0900

    양방향 참조 예제 프로젝트

---
+++ bimovie/src/main/java/org/zerock/bimovie/BimovieApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.bimovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BimovieApplication.class, args);
+    }
+
+}
