86699fb14454945885222cf5c39527bdafa9eb05
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-04-06 12:07:38 +0900

    useMemo

--- Spring Boot With React/REACTWORK/my-app/src/App.js
+++ Spring Boot With React/REACTWORK/my-app/src/App.js
@@ -1,41 +1,41 @@
-import { cleanup } from '@testing-library/react' 
-import { useEffect, useState } from 'react' // {}는 해당 파일에 메인 오브젝트가 아닌 다른것을 임포트한다는 의미 -> export default로 가져오는게 아니라는 의미
-import './App.css' 
 
-// map, filter, concat, spread, slice
+// useMemo => 메모라이제이션(기억)
+// React Hooks
+
+import { useMemo, useState } from "react";
+
 function App(){
 
-  const[data, setData] = useState(0);
-  const[search, setsearch] = useState(0);
+  const[list, setList] = useState([1,2,3,4]);
+  const[str, setStr] = useState("합계");
+  
 
-  const download = () => {
-    // 다운로드 받고 통신
-    let downloadData = 5; // 가정
-    setData(downloadData);
+
+  const getAddResult = () =>{
+    //1 초
+    let sum = 0;
+    list.forEach((i) => (sum = sum + i));
+    console.log('sum', sum);
+    return sum;
   }
+  const addResult = useMemo(()=>getAddResult(),[list]);
 
-// 실행시점: 
-// 1. App() 그림이 최초 그려질 때
-// 2. 상태 변수가 변경될 때 (dependencyList에 등록되어 있어야함!)
-// 3. 의존리스트 관리 가능
-useEffect(()=>{
-  console.log("useEffect 실행");
-  download();
-}, [search]);
+return <div>
+ <button onClick={()=>{setStr("안녕");
+  }}> 리스트값 추가
+  </button>
 
-return (
-<div>
-  <button onClick={()=>{
-    setsearch(2);
-  }}>검색하기</button>
-  <h1>Hello Word : {data}</h1>
-  <button onClick={()=>{
-    setData(data+1);
-  }}>더하기
-</button>
+  <button onClick={()=>{setList([...list, 10]);
+  }}> 리스트값 추가
+  </button>
+  <div>
+  {list.map((i) => (
+    <h1>{i}</h1>
+  ))}
+  </div>
+  <div>{str} : {addResult}</div>
 </div>
-);
+
 }
 
-// 해당 함수를 외부로 노출
 export default App;
