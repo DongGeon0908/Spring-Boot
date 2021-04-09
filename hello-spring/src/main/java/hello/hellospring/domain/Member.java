3907cc4f83cfdb659e53e124fce64a3a8323203e
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-04-09 13:10:47 +0900

    회원 도메인과 레포지토리 구현

---
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,25 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    // 데이터를 구별하기 위한 식별자
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
