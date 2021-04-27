feaf1f1a1f24659c994565218de73c0c7dcae67c
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-04-27 17:21:45 +0900

    Spring Transaction

---
+++ hello-spring/src/main/java/hello/hellospring/repository/JdbcTemplateMembersRepository.java
@@ -0,0 +1,52 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcTemplateMembersRepository implements MemberRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired // 생성자가 클래스에 딱 하나만 존재한다면, @Autowired 생략 가능
+    public JdbcTemplateMembersRepository(DataSource dataSource) {
+        this.jdbcTemplate = new jdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result =  jdbcTemplate.query("select * from member where d = ?", memberRowMapper());
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+
+    private RowMapper<Member> memberRowMapper(){
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
+    }
+}
