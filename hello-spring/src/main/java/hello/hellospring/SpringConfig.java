da2f6c3f3ec2885021e52f6f2413a67f4e4c68bb
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-04-28 22:34:07 +0900

    Spring Data Jpa 적용

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -12,27 +12,16 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em){
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-
-        return new JpaMemberRepository(em);
-    }
-
-    // 객체지향의 다형성이 좋은점?
-    // 스프링부트에서는 객체지향의 다형성을 활용해서 객체를 바꾸기만 해도 다른 설정을 바꿀 필요없이 서비스를 변경할 수 있다.
 
 }
