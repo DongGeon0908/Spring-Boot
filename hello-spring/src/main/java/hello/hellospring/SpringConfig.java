e3bb2bedf83e25a05fa4139f7b2c66e9755c9656
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-04-28 22:58:41 +0900

    AOP 기초

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,18 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
+@Component
 public class SpringConfig {
 
     private final MemberRepository memberRepository;
@@ -23,5 +26,6 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
 
 }
