e5240bf5a3297294ac3c7b3c510182d610957d34
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-05-24 21:25:24 +0900

    Command Object를 활용한 Controller 구현

--- lec17Pjt/src/main/java/com/bs/lec17/member/controller/MemberController.java
+++ lec17Pjt/src/main/java/com/bs/lec17/member/controller/MemberController.java
@@ -12,50 +12,41 @@ import com.bs.lec17.member.Member;
 import com.bs.lec17.member.service.MemberService;
 
 @Controller
+// @RequestMapping("공통부문")
 public class MemberController {
 
 	// qk
-	
-	
+
 	@Autowired
 	MemberService service;
-	
-	@RequestMapping(value="/memJoin", method=RequestMethod.POST)
-	public String memJoin(Model model, HttpServletRequest request) {
-		String memId = request.getParameter("memId");
-		String memPw = request.getParameter("memPw");
-		String memMail = request.getParameter("memMail");
-		String memPhone1 = request.getParameter("memPhone1");
-		String memPhone2 = request.getParameter("memPhone2");
-		String memPhone3 = request.getParameter("memPhone3");
-		
-		service.memberRegister(memId, memPw, memMail, memPhone1, memPhone2, memPhone3);
-		
-		model.addAttribute("memId", memId);
-		model.addAttribute("memPw", memPw);
-		model.addAttribute("memMail", memMail);
-		model.addAttribute("memPhone", memPhone1 + " - " + memPhone2 + " - " + memPhone3);
-		
+
+	// Post 방식일때는 POST임을 명시해야함!!
+	// Post 방식일때는 POST임을 명시해야함!!
+	@RequestMapping(value = "/memJoin", method = RequestMethod.POST)
+	public String memJoin(Member member) {
+
+		service.memberRegister(member.getMemId(), member.getMemPw(), member.getMemMail(), member.getMemPhone1(),
+				member.getMemPhone2(), member.getMemPhone3());
+
 		return "memJoinOk";
 	}
-	
-	@RequestMapping(value="/memLogin", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/memLogin", method = RequestMethod.POST)
 	public String memLogin(Model model, HttpServletRequest request) {
-		
+
 		String memId = request.getParameter("memId");
 		String memPw = request.getParameter("memPw");
-		
+
 		Member member = service.memberSearch(memId, memPw);
-		
+
 		try {
 			model.addAttribute("memId", member.getMemId());
 			model.addAttribute("memPw", member.getMemPw());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		return "memLoginOk";
 	}
-	
+
 }
--- lec17Pjt/src/main/webapp/WEB-INF/views/memJoinOk.jsp
+++ lec17Pjt/src/main/webapp/WEB-INF/views/memJoinOk.jsp
@@ -6,10 +6,10 @@
 </head>
 <body>
 	<h1> memJoinOk </h1>
-	ID : ${memId}<br />
-	PW : ${memPw}<br />
-	Mail : ${memMail} <br />
-	Phone : ${memPhone} <br />
+	ID : ${member.memId}<br />
+	PW : ${member.memPw}<br />
+	Mail : ${member.memMail} <br />
+	Phone : ${member.memPhone1} ${member.memPhone2} ${member.memPhone3}<br />
 	
 	<a href="/lec17/resources/html/memJoin.html"> Go MemberJoin </a>
 </body>
