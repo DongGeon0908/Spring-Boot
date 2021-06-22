d1ec8d2f164d5355a297a99d2dbb44e304841bf9
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-06-22 12:02:32 +0900

    User // Order

---
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    // 1000원 할인
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
