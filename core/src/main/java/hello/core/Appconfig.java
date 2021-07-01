b34c677d8622a999fd6afd8899160831b7149daf
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-07-01 17:23:06 +0900

    Appconfig 리펙터링

--- core/src/main/java/hello/core/Appconfig.java
+++ core/src/main/java/hello/core/Appconfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -11,13 +12,20 @@ public class Appconfig {
 
     public MemberService memberService() {
         // 생성자 주입 <- DI
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
         // 생성자 주입 <- DI
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 }
