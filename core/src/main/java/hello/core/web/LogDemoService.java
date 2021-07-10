295c29ae0281e6f3223e074843d3852605bf607c
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-07-10 18:46:26 +0900

    scopse and provider

--- core/src/main/java/hello/core/web/LogDemoService.java
+++ core/src/main/java/hello/core/web/LogDemoService.java
@@ -2,15 +2,17 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor // 자동으로 생성자를 만들어준다잉~
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
