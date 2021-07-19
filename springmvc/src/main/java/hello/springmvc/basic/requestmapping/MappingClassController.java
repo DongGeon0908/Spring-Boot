37c2cc4d58a63357e0a7c19d8e6640645ecdd7da
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-07-19 16:34:58 +0900

    HTTP Header

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -1,2 +1,33 @@
-package hello.springmvc.basic.requestmapping;public class MappingClassController {
+package hello.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+    @GetMapping
+    public String user() {
+        return "get users";
+    }
+
+    @PostMapping
+    public String addUser() {
+        return "post user";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userID) {
+        return "get userId = " + userID;
+    }
+
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable String userID) {
+        return "update userId = " + userID;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userID) {
+        return "update userId = " + userID;
+    }
 }
