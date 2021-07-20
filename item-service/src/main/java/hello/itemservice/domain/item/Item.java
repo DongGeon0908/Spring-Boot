0eb03928bfc7ef867566a5b4e4bd64a37a925d02
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-07-20 17:21:14 +0900

    상품 도메인 개발

---
+++ item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,26 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Data <- 매우 위험함...
+@Getter
+@Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
