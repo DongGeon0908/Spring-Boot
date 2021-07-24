232f7ab52758c396d71013874d746eed16370f47
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-07-24 16:58:25 +0900

    상품 서비스 개발

---
+++ jpabook/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.respository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * @param item
+     * @Transactional 은 default로 readOnly = false 임당!
+     */
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+}
