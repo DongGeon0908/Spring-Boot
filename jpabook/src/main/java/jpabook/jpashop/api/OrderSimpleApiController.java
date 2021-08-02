1b76321af308e38e7f3dc1de271dd9f55cca28bb
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-08-02 18:02:15 +0900

    양방향 연관관계에서 발생하는 무한로프 에러

---
+++ jpabook/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.respository.OrderRepository;
+import jpabook.jpashop.respository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    // 무한루프 에러...
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+
+}
