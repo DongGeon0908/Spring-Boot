6f3df86f1cf2c49c5a51df32ee300bb963cd515c
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-08-03 17:42:55 +0900

    컬렉션 조회 최적화

--- jpabook/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpabook/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -64,6 +64,11 @@ public class OrderApiController {
     }
 
     @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV4() {
         return orderQueryRepository.findOrderQueryDtos();
     }
