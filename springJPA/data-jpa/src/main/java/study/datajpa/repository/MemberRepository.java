d0306fc01c63bf3c29cbb9a5411a7b3cd156cfab
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-08-23 23:50:03 +0900

    공통 인터페이스 분석

--- springJPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ springJPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,7 +1,12 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsername(String username);
 }
