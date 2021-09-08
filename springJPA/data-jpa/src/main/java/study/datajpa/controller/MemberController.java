1ca09069340bc949a7cbb079edd3525393aba09f
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-09-08 17:58:06 +0900

    .

---
+++ springJPA/data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,28 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    // 도메인 클래스 컨버터 기능은 권장하지 않음
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+
+}
